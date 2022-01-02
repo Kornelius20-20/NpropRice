7b08ea2e3bed5c55e84f641e49fde8bc44b1af5b
Kornelius20-20 <weeraman97@gmail.com>
2022-01-02 16:59:48 +0530

    Removed venv and older versions of changed files

--- makeNetwork.py
+++
@@ -1,5 +0,0 @@
-import NetCandPred,os
-from networkx.algorithms.link_analysis import pagerank
-network = NetCandPred.NetCandPred(os.path.join("txt/output_file.txt"),delimitter=' ')
-
-print(pagerank(network.graph))
