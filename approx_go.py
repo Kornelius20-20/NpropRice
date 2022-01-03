3f68f60a2e6acf0150da0688642fa96f04ccbec9
Janith <weeraman97@gmail.com>
2022-01-03 13:56:16 +0530

    Created script that takes GO terms associated with proteins that have them, finds the most common GO term between a node and neighbors, and assigns it to node

--- approx_go.py
+++ approx_go.py
@@ -2,6 +2,7 @@ import networkx as nx
 import pandas as pd
 from collections import Counter
 
+
 graphfile = "drought_module2.gexf"
 dframe = "txt/processed_uniprot.csv"
 
@@ -10,35 +11,43 @@ frame = pd.read_csv(dframe)
 
 import re
 
+# Add GO terms from the GO columns into a 'GO' attribute as a set
 for node in graph.nodes:
     row = frame[frame['Entry'] == node]
 
     if not row.empty:
+        # For every node who is present in the uniprot data, get GO IDs
         biogo = row["Gene ontology (biological process)"].to_string() + row["Gene ontology (GO)"].to_string()
         biogo += row["Gene ontology (molecular function)"].to_string() + row["Gene ontology IDs"].to_string()
+        # Extract just the GO terms and form them into a non-redundant set
         res = re.findall("GO:\d\d\d\d\d\d\d", biogo)
         goset = set(res)
 
-        toadddict = {node:{'GO': goset}}
-        nx.set_node_attributes(graph, toadddict)
+        # Add to the 'GO' attribute
         graph.nodes[node]['GO'] = goset
 
-# Get all the associated GO terms for each node and add them as a list
 
+# Iterate over all nodes, getting nodes and their attributes
 for node,attrs in graph.nodes(True):
+    # For nodes with GO terms in attributes
     if 'GO' in attrs:
-
+        # Get the GO terms
         nodego = attrs['GO']
         combinedgos = []
+
+        # For each neighbor of node
         for neighbor in graph.neighbors(node):
+            # Try to get the neighbors GO terms if it exists
             try:
                 neighborgo = graph.nodes[neighbor]['GO']
             except KeyError:
                 continue
 
+            # Find GO terms that are in common with neighbor and add to combinedgos list
             intersected = nodego.intersection(neighborgo)
             combinedgos += list(intersected)
 
+        # Count the occurences of each GO term in the list and assign 'GO' value as highest value in list
         golabels = Counter(combinedgos)
         mostcomlabel = golabels.most_common(1)
         if len(mostcomlabel) > 0:
@@ -47,9 +56,4 @@ for node,attrs in graph.nodes(True):
         else:
             graph.nodes[node]['GO'] = list(graph.nodes[node]['GO'])[0]
 
-# for node,attrs in graph.nodes(True):
-#     if 'GO' in attrs:
-#         print(node,attrs['GO'])
 
-
-nx.write_gexf(graph,"testgraph.gexf")
--- gene_ontology_gexf.py
+++ gene_ontology_gexf.py
@@ -4,8 +4,6 @@ import re
 
 graphfile = "drought_module.gexf"
 dframe = "txt/processed_uniprot.csv"
-interested_go = {"GO:0009415", 'GO:0009270', 'GO:0009413', 'GO:0009414', 'GO:0030912', 'GO:0051599', 'GO:0071462',
-                 "GO:0009738", "GO:0009787", "GO:1901527", "GO:0009788", "GO:0009789" }
 
 graph = nx.read_gexf(graphfile)
 frame = pd.read_csv(dframe)
@@ -19,16 +17,16 @@ for prot in graph.nodes:
         else:
             attributes = {'status':status['Status'].to_string()[-8:]}
 
-            # Get GO IDs and remove the row number from the first index
-            go_process = status["Gene ontology (biological process)"].to_string() \
-                         + status["Gene ontology IDs"].to_string()
-            # finding the GO IDs via regex search
-            regex_results = re.findall("GO:\d\d\d\d\d\d\d", go_process)
-
-            for item in regex_results:
-                if item in interested_go:
-                    attributes['GO'] = item
-                    break
+            # # Get GO IDs and remove the row number from the first index
+            # go_process = status["Gene ontology (biological process)"].to_string() \
+            #              + status["Gene ontology IDs"].to_string()
+            # # finding the GO IDs via regex search
+            # regex_results = re.findall("GO:\d\d\d\d\d\d\d", go_process)
+            #
+            # for item in regex_results:
+            #     if item in interested_go:
+            #         attributes['GO'] = item
+            #         break
 
             # response to osmotic stress GO:0006970
             nodes[prot] = attributes
