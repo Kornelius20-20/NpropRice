39c4ee35a6b87299f20b90eb793ec5e7d1283f1e
BLACKBOX <weeraman97@gmail.com>
2022-04-05 10:41:37 +0530

    Added multiple GO attributes to each node in the graph to hold each of the go values associated with it up to a maximum of 5

--- test.py
+++ test.py
@@ -3,6 +3,36 @@ from networkx.algorithms.community import greedy_modularity_communities
 import approx_go
 import pandas as pd
 
+def descendingdictkeys(dic,desc=True):
+       """
+       Method that takes in a dictionary with keys and values that are integers and returns the keys in descending order
+       of the values
+
+       Parameters
+       ----------
+       dic: input dictionary
+
+       Returns
+       -------
+       list of keys sorted based on order specified
+
+       """
+
+       keys, values = zip(*dic.items())
+       keys = list(keys)
+       values = list(values)
+
+       output = []
+       while len(values) > 0:
+              bestind = values.index(max(values))
+              output.append(keys[bestind])
+              keys.pop(bestind)
+              values.pop(bestind)
+
+       if desc:
+              return output
+       else: return output.reverse()
+
 
 dframe2 = "txt/uniprot_original.tsv"
 graph = nx.read_gexf('outputs/graphs/p-10-0.1-50seedsAndGO.gexf')
@@ -13,13 +43,34 @@ graph = approx_go.assign_metadata(graph, frame, asterm=False)
 results = greedy_modularity_communities(graph)
 
 for i in range(len(results)):
-       for node in results[i]:
+       gos_in_clust = {}
 
+       # For nodes with GO terms, score their terms in the cluster GO list
+       for node in results[i]:
               try:
-                     print(graph.nodes[node]['GO'])
+                     assignedgo = graph.nodes[node]['GO']
+                     if type(assignedgo) is str:
+                            score = gos_in_clust.get(assignedgo,0)
+                            gos_in_clust[assignedgo] = score + 1
+                     else:
+                            for go in assignedgo:
+                                   score = gos_in_clust.get(go, 0)
+                                   gos_in_clust[go] = score + 1
+
+              except KeyError:
+                     continue
+
+       orderedgos = descendingdictkeys(gos_in_clust)
+
+       for node in results[i]:
+              try:
+                     del graph.nodes[node]['GO']
+                     for i in range(0,5):
+                            goterm = f'GO{i+1}'
+                            graph.nodes[node][goterm] = orderedgos[i]
+
               except KeyError:
                      continue
 
 
-
-# nx.write_gexf(graph,'outputs/graphs/p-10-0.1-50seedsAndGO-w-community.gexf')
+nx.write_gexf(graph,'outputs/graphs/p-10-0.1-50seedsAndGO-w-community.gexf')
