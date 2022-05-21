853850a3b58b63671f1735d1bfa794ce8d31f277
Kornelius20-20 <weeraman97@gmail.com>
2022-05-21 18:42:37 +0530

    adding code for a csv that puts together candidates

--- MarkCands.py
+++ MarkCands.py
@@ -3,7 +3,7 @@ import os
 
 outputdir = "outputs/graphs/"
 algorithms = ['greedy_clusters','label_propagation','louvain']
-cuts = [60]
+cuts = [i for i in range(25,75,5)]
 
 
 from cluster_drought_module_greedy import get_best_scoring_nodes,transpose_lists
@@ -35,11 +35,23 @@ for _,_,files in os.walk(os.path.join(outputdir)):
     for file in files:
         graph = nx.read_gexf(os.path.join(outputdir,file))
 
+        commonprots = []
         for cut in cuts:
             commons = get_cands_over_cutoff(graph,algorithms,cut)
             add_cutoff_nodes(graph,commons,cut)
+            commons.sort()
+            commonprots.append(commons)
 
+        with open(os.path.join("outputs/results",f"candsfor{file[:-5]}.csv"),'w') as outfile:
+            lines = transpose_lists(commonprots)
+            lines.insert(0,[str(i) for i in cuts])
+            for line in lines:
+                outfile.writelines(','.join(line))
+                outfile.write('\n')
         nx.write_gexf(graph,os.path.join(outputdir,file))
+
+
+
 
 def get_cand_scores(graph,candidates,attrs,output='canddata.csv'):
 
@@ -63,8 +75,6 @@ def get_cand_scores(graph,candidates,attrs,output='canddata.csv'):
             line = [str(i) for i in line]
             file.writelines(','.join(line)+'\n')
 
-
-
 get_cand_scores(graph,commons,algorithms)
 
 
@@ -86,34 +96,3 @@ def tsv_gene_clusters(graph,attr):
         for line in clusterlist:
             file.writelines('\t'.join(line)+'\n')
 
-def getGOofClusters():
-    from suds.client import Client
-
-    # Getting functional annotation chart from DAVID for a gene list and figuring out how to parse it
-    genetype = "UNIPROT_ID"
-    tool = "term2term"
-    annotlist = ['GOTERM_BP_FAT','GOTERM_CC_FAT','GOTERM_MF_FAT','INTERPRO','KEGG_PAHWAY']
-    idlist = ['Q6K9M5_ORYSJ', 'Q93X08_ORYSJ', 'Q0J0N3_ORYSJ', 'SUS3_ORYSJ', 'C7J2A4_ORYSJ', 'SUS4_ORYSJ', 'SUS5_ORYSJ',
-              'SPSA3_ORYSJ', 'SPSA4_ORYSJ', 'Q6ZGL5_ORYSJ', 'SPSA5_ORYSJ', 'Q6ERD9_ORYSJ', 'Q8GTK0_ORYSJ', 'Q2MJA8_ORYSJ',
-              'SPSA1_ORYSJ', 'A0A0P0V0U6_ORYSJ', 'Q75II7_ORYSJ', 'Q6ZCH3_ORYSJ', 'Q2MJA7_ORYSJ', 'Q10P27_ORYSJ',
-              'Q7F5L2_ORYSJ', 'SUS7_ORYSJ', 'Q5JNJ1_ORYSJ', 'A0A0N7KG85_ORYSJ', 'Q9AX07_ORYSJ', 'Q0DTU6_ORYSJ',
-              'A0A0P0UZR1_ORYSJ', 'Q6Z548_ORYSJ', 'SPSA2_ORYSJ', 'SSG1_ORYSJ', 'SUS2_ORYSJ', 'SUS1_ORYSJ', 'SUS6_ORYSJ',
-              'Q0DDZ4_ORYSJ']
-
-    annot = ','.join(annotlist)
-    ids = ','.join(idlist)
-    listType = 0
-
-    serviceurl = 'https://david.ncifcrf.gov/webservice/services/DAVIDWebService?wsdl'
-
-    # Create service client using WSDL
-    client = Client(serviceurl)
-    client.wsdl.services[0].setlocation('https://david.ncifcrf.gov/webservice/services/DAVIDWebService.DAVIDWebServiceHttpSoap11Endpoint/')
-
-    # authenticate
-    client.service.authenticate('2017s16486@stu.cmb.ac.lk')
-
-    client.service.addList(ids, genetype, "testlist", listType)
-    result = client.service.getTermClusterReport()
-
-    print(result)
