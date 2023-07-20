9be8a5dffc6a3772500f5f6bf03a998969b6aec1
Kornelius20-20_Lap <weeraman97@gmail.com>
2023-07-20 16:16:26 +0530

    Re-wrote netprop to fig a bug in sparse array gen

--- newnetprop.py
+++ newnetprop.py
@@ -17,20 +17,74 @@ def weights_from_seeds(graph, seeds):
 
     p0 = []
 
-    propability = float(1 / len(seeds))
-
     for name in graph.nodes:
         if name in seeds:
-            p0.append(propability)
+            p0.append(nx.degree(graph,name))
         else:
             p0.append(0)
 
     p0 = csc_array(p0)
 
     return p0.transpose()
+def netprop(graph,seeds,alpha = 0.7,num_iters=0,threshold = 1.0e-10):
+    """
+    Given a networkx graph and a list of seeds, assigns a weight to each seed and propagates it out into
+    the network for a certain number of iterations or until a threshold for the sum of node weights is 
+    reached.
+
+    :param graph: input graph
+    :param seedlist: list of seed nodes
+    :param alpha: learning rate
+    :param iter: number of iterations to restart the walk
+    :param threshold: limit of change of weight at which the rwr is stopped
+    :return: numpy array of final weights of nodes
+    
+    """
 
 
-def netprop(graph,seeds,alpha = 0.01,iter=0,threshold = 1.0e-6):
+    A = nx.adjacency_matrix(graph).tocsc()
+    A = csc_array(A,dtype="float64")
+
+    # Normalize adjacency matrix
+    for i in range(A.shape[1]):
+        colsum = A.data[A.indptr[i]:A.indptr[i + 1]].sum()
+
+        if colsum > 0:
+            A.data[A.indptr[i]:A.indptr[i + 1]] = (A.data[A.indptr[i]:A.indptr[i + 1]]/colsum)
+
+
+    p0 = weights_from_seeds(graph,seeds)
+
+    p = alpha * p0
+    const_fact = p.copy()
+
+    if num_iters > 0:
+        for i in range(num_iters):
+            _p = p.copy()
+            p = const_fact + (1-alpha)*A.dot(p)
+            print(f"change in probability: {p.sum() - _p.sum()}")
+
+    else:
+        num_iters = 1
+
+        p = const_fact + (1-alpha)*A.dot(p)
+        _p = csc_array(p.shape, dtype=float)
+
+        while (p.sum() - _p.sum()) > threshold:
+            _p = p.copy()
+            p = const_fact + (1-alpha)*A.dot(p)
+            num_iters += 1
+
+            print(f"change in probability: {p.sum() - _p.sum()}")
+
+
+    print(f"RWR ran for {num_iters} iterations")
+
+
+    return p.toarray()
+
+
+def netprop(graph,seeds,alpha = 0.01,num_iters=0,threshold = 1.0e-6,starting_prob = 0):
     """
     Given a networkx graph and a list of seeds, assigns a weight to each seed and propagates it out into
     the network for a certain number of iterations or until a threshold for the sum of node weights is 
@@ -53,16 +107,18 @@ def netprop(graph,seeds,alpha = 0.01,iter=0,threshold = 1.0e-6):
         colsum = A.data[A.indptr[i]:A.indptr[i + 1]].sum()
 
         if colsum > 0:
-            A.data[A.indptr[i]:A.indptr[i + 1]] /= colsum
+            A.data[A.indptr[i]:A.indptr[i + 1]] = (A.data[A.indptr[i]:A.indptr[i + 1]]/colsum)
 
 
-    p0 = weights_from_seeds(graph,seeds)
+    p0 = weights_from_seeds(graph,seeds,starting_prob)
 
-    p = alpha * p0
+    p = alpha * p0.transpose()
 
-    if iter > 0:
-        for i in range(iter):
-            p = alpha*p0 + (1-alpha)*A *p
+    if num_iters > 0:
+        for i in range(num_iters):
+            _p = p.copy()
+            p = alpha*p0.transpose() + (1-alpha)*A *p.transpose()
+            print(f"change in probability: {p.sum() - _p.sum()}")
 
     else:
         num_iters = 1
@@ -72,8 +128,11 @@ def netprop(graph,seeds,alpha = 0.01,iter=0,threshold = 1.0e-6):
 
         while (p.sum() - _p.sum()) > threshold:
             _p = p.copy()
-            p = alpha * p0 + (1 - alpha) * A * p
+            p = alpha * p0.transpose() + (1 - alpha) * A * p.transpose()
             num_iters += 1
+
+            print(f"change in probability: {p.sum() - _p.sum()}")
+
 
     print(f"RWR ran for {num_iters} iterations")
 
